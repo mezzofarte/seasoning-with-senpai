5982f01e33318be89dab7b46f2398b05626960b6
Dillon Vuong <dillonv@uci.edu>
2019-02-26 13:45:29 -0800

    added  Particle Collision Script

---
+++ Salt Assault/Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public ParticleSystem part;
+    // Start is called before the first frame update
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = particleSystem.GetCollisionEvents(other, collisionEvents);
+        if (other.CompareTag("Player"))
+        {
+            
+        }
+
+    }
+    
+}
