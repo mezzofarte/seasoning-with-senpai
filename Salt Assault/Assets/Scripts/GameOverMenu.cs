6e4b289d66575cfcfb01d628ed7aeed1cbf8b102
patrickluong <39811672+patrickluong@users.noreply.github.com>
2019-02-25 22:02:27 -0800

    Added game over screen

---
+++ Salt Assault/Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    
+    public void PlayAgain()
+    {
+        // Replace "Game" with name of first level
+        // when game is complete
+        //
+        // SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
